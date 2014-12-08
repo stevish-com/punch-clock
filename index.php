76ad49ab55e06ac155ee0e191d924eed4f2cfb28
Stephen Narwold <stevish@gmail.com>
2014-12-07 20:30:59 -0800

    Kill dumb session stuff, save password, make mobile version for my phone

--- index.php
+++ index.php
@@ -20,6 +20,22 @@ Stevish Punch Clock in PHP
 */
 session_start();
 
+//Rebuild Post from Session
+$rebuildable = array(
+	'report_job',
+	'from_date',
+	'to_date'
+	);
+if ( $_POST['action'] != 'update_report' ) {
+	foreach( $rebuildable as $key ) {
+		$_POST[ $key ] = $_SESSION[ 'post-' . $key ];
+	}
+} else {
+	foreach( $rebuildable as $key ) {
+		$_SESSION[ 'post-' . $key ] = $_POST[ $key ];
+	}
+}
+
 include_once("db_config.php");
 $now = time();
 if(!$sql_user || !$sql_pass || !$sql_db) {
@@ -32,10 +48,15 @@ if (mysqli_connect_error()) {
 
 $status = array(0 => 'unpaid', 1 => 'invoiced', 2=> 'paid');
 $pass_hash = $mysqli->query("SELECT `value` FROM `data` WHERE `name` = 'pass';")->fetch_object()->value;
-$thisjob = intval($_POST['job']);
+$thisjob = intval($_POST['report_job']);
 $thisrate = $mysqli->query("SELECT `rate` FROM `jobs` WHERE `id` = '$thisjob';")->fetch_object()->rate;
 $from_date = preg_replace("/[^0-9\/]/", "", $_POST['from_date']);
 $to_date = preg_replace("/[^0-9\/]/", "", $_POST['to_date']);
+
+//Save Password
+if ( $_POST[ 'password' ] ) {
+	setrawcookie( 'pass', s_hash( $_POST[ 'password' ] ), time() + 60*60*24*30 );
+}
 
 if ( isset( $from_date ) ) {
 	$_SESSION['from_date'] = $from_date;
@@ -46,7 +67,14 @@ if ( isset( $to_date ) ) {
 
 //Get Jobs
 $jobs_result = $mysqli->query("SELECT * FROM `jobs` left join `inout` on `jobs`.`id` = `inout`.`job` WHERE `jobs`.`inactive` != 1 GROUP BY `inout`.`job` ORDER BY max(`inout`.`in`) DESC;");
-
+$job_options = '';
+while($job = $jobs_result->fetch_assoc()) {
+	if($job['id'] == $thisjob) {
+		$job_options .= "<option value='{$job['id']}' selected='selected'>{$job['name']}</option>\n";
+	} else {
+		$job_options .= "<option value='{$job['id']}'>{$job['name']}</option>\n";
+	}
+}
 
 if($clockedin = $mysqli->query("SELECT `value` FROM `data` WHERE `name` = 'clockedin';")->fetch_object()->value) {
 	$clockedintime = date('H:i:s m/d/Y', $clockedintimestamp = $mysqli->query("SELECT `in` FROM `inout` WHERE `punch_id` = '$clockedin';")->fetch_object()->in);
@@ -58,104 +86,30 @@ if (intval($_GET['co_id'])) {
 switch($_POST['action']) {
 	case 'get_time_in':
 		if ($clockedin) {
-			die( h2hm( ( $now - $clockedintimestamp ) / 3600) );
+			die( /*h2hm( ( $now - $clockedintimestamp ) / 3600)*/ );
 		} else {
 			die("0");
 		}
 		break;
 	case 'in':
-		if ( s_hash( $_POST[ 'pass' ] ) != $pass_hash ) {
+		if ( s_hash( $_POST[ 'password' ] ) != $pass_hash && $_COOKIE[ 'pass' ] != $pass_hash ) {
 			die("Bad password. Go back and try again");
 		}
 		if(!$clockedin) {
 			clock_in();
 		} else {
-			error("clockedinout did not match");
+			error("Already clocked in...");
 		}
 		break;
 	case 'out':
-		if ( s_hash( $_POST[ 'pass' ] ) != $pass_hash ) {
+		if ( s_hash( $_POST[ 'password' ] ) != $pass_hash && $_COOKIE[ 'pass' ] != $pass_hash ) {
 			die("Bad password. Go back and try again");
 		}
 		if($clockedin) {
 			clock_out($clockedin);
 		} else {
-			error("clockedinout did not match");
+			error("Already clocked out...");
 		}
-		break;
-	case 'update_report':
-
-		if($thisjob) {
-			$where = "WHERE `job` = '$thisjob' AND `in` > '0' AND `out` > '0'";
-			if($from_date) {
-				$fds = explode("/", $from_date);
-				$from_date_unix = mktime(0,0,0,$fds[0],$fds[1],$fds[2]);
-				$where .= " AND `in` >= '$from_date_unix'";
-			}
-			if($to_date) {
-				$tds = explode("/", $to_date);
-				$to_date_unix = mktime(23,59,59,$tds[0],$tds[1],$tds[2]);
-				$where .= " AND `in` <= '$to_date_unix'";
-			}
-			$result = $mysqli->query("SELECT * FROM `inout` $where ORDER BY `in` ASC");
-			$jobdata = array();
-			$paidhours = $paidearned = $unpaidhours = $unpaidearned = $invoicedhours = $invoicedearned = 0;
-			while($inout = $result->fetch_assoc()) {
-				$seconds = $inout['out'] - $inout['in'];
-				$hours = $seconds / 3600;
-				$inout['hours'] = h2hm($hours);
-				$inout['earned'] = $hours * $thisrate;
-				
-				$statushours = $status[$inout['paid']] . 'hours';
-				$statusearned = $status[$inout['paid']] . 'earned';
-				
-				$jobdata[$status[$inout['paid']]][] = $inout;
-				$$statushours += $hours;
-				$$statusearned += $inout['earned'];
-			}
-		}
-		
-		ob_start();
-		foreach($status as $s) {
-			$shours = $s . 'hours';
-			$searned = $s . 'earned';
-			if(count($jobdata[$s]) > 0) {
-				?>
-				<table border="1" cellspacing="2" cellpadding="5">
-					<tr><th colspan="4"><?php echo strtoupper($s); ?></th></tr>
-					<tr><th>In</th><th>Out</th><th>Hours</th><th>Earned</th><th>Change to...</th></tr>
-					<?php
-						foreach( $jobdata[$s] as $inout ) {
-							echo '<tr><td>' . date("m/d/Y H:i:s", $inout['in']) . '</td><td>' . date("m/d/Y H:i:s", $inout['out']) . '</td><td>' . $inout['hours'] . '</td><td>' . $inout['earned'] . '</td>';
-							echo '<td><form method="POST"><input type="hidden" name="action" value="edit_status" /><input type="hidden" name="punch_id" value="' . $inout['punch_id'] . '" />';
-							if ( 'unpaid' != $s ) {
-								echo '<input type="submit" name="status" value="unpaid" />';
-							}
-							if ( 'invoiced' != $s ) {
-								echo '<input type="submit" name="status" value="invoiced" />';
-							}
-							if ( 'paid' != $s ) {
-								echo '<input type="submit" name="status" value="paid" />';
-							}
-							echo '</form></td></tr>';
-						}
-						echo '<tr><td></td><th>Totals</th><th>' . h2hm($$shours) . '</th><th>$' . number_format($$searned, 2) . '</th></tr>';
-					?>
-				</table>
-				<?php
-			}
-		}
-		$_SESSION['report'] = ob_get_contents();
-		$_SESSION['report_job_options'] = '';
-		while($job = $jobs_result->fetch_assoc()) {
-			if($job['id'] == $thisjob) {
-				$_SESSION['report_job_options'] .= "<option value='{$job['id']}' selected='selected'>{$job['name']}</option>\n";
-			} else {
-				$_SESSION['report_job_options'] .= "<option value='{$job['id']}'>{$job['name']}</option>\n";
-			}
-		}
-		ob_end_clean();
-		
 		break;
 	case "edit_status":
 		$punch_id = intval( $_POST[ 'punch_id' ] );
@@ -172,25 +126,17 @@ switch($_POST['action']) {
 		break;
 }
 
-if($_POST['action'] != 'update_report') {
-	$_SESSION['job_options'] = '';
-	while($job = $jobs_result->fetch_assoc()) {
-		if($job['id'] == $thisjob) {
-			$_SESSION['job_options'] .= "<option value='{$job['id']}' selected='selected'>{$job['name']}</option>\n";
-		} else {
-			$_SESSION['job_options'] .= "<option value='{$job['id']}'>{$job['name']}</option>\n";
-		}
-	}
-	if(!$_SESSION['report_job_options']) {
-		$_SESSION['report_job_options'] = $_SESSION['job_options'];
-	}
-}
-
 if($clockedin) {
 	echo "Clocked in at $clockedintime<br/><br/>";
 }
 ?>
-<script type="text/javascript">
+<head>
+	<title>Punch Clock</title>
+	<?php if ( stripos( $_SERVER[ 'HTTP_USER_AGENT' ], 'android' ) ) { ?>
+		<link rel="stylesheet" type="text/css" href="mobile.css">
+	<?php } ?>
+</head>
+<!--<script type="text/javascript">
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
@@ -204,25 +150,32 @@ if($clockedin) {
 		xmlhttp.send("action=get_time_in");
 	}
 
-</script>
-<form method="POST">
-	Time: <input type="text" size="2" value="<?php echo date("H", $now); ?>" name="h" />:<input type="text" size="2" value="<?php echo date("i", $now); ?>" name="m" /> (<input type="text" size="2" value="<?php echo date("m", $now); ?>" name="mo" />/<input type="text" size="2" value="<?php echo date("d", $now); ?>" name="d" />/<input type="text" size="4" value="<?php echo date("Y", $now); ?>" name="y" />)<br/>
+</script>-->
+
+<form method="POST" id="punchform">
+	<span id="time_input" class="formline">Time: <input type="text" size="2" value="<?php echo date("H", $now); ?>" name="h" />:<input type="text" size="2" value="<?php echo date("i", $now); ?>" name="m" /></span> <span id="date_input" class="formline">(<input type="text" size="2" value="<?php echo date("m", $now); ?>" name="mo" />/<input type="text" size="2" value="<?php echo date("d", $now); ?>" name="d" />/<input type="text" size="4" class="digit4" value="<?php echo date("Y", $now); ?>" name="y" />)<br/></span>
 	<?php if($clockedin) {
 		$jobin = $mysqli->query("SELECT `name` FROM `jobs` WHERE `id` = '" . $mysqli->query("SELECT `job` FROM `inout` WHERE `punch_id` = '$clockedin';")->fetch_object()->job . "';")->fetch_object()->name;
 		echo "Clocked in for $jobin (<span id='time_in' onclick='refreshTimeIn()'>" . h2hm(($now - $clockedintimestamp) / 3600) . "</span>)<br/>";
 	} else { ?>
 		Job: <select name="job">
 			<?php
-				echo $_SESSION['job_options'];
+				echo $job_options;
 			?>
 			<option value="0">Add New...</option>
-		</select> <input type="text" name="newjob" />  $<input type="text" name="newrate" size="3" /><br/>
+		</select> <span id="newjob_input"><input type="text" class="text" name="newjob" />  $<input type="text" class="newrate" name="newrate" size="3" /><br/></span>
 	<?php } ?>
 	<input type="hidden" name="action" value="<?php echo $clockedin ? 'out' : 'in'; ?>" />
-	<label for="username">User: </label><input type="text" name="username" /><br/>
-	<label for="pass">Pass: </label><input type="password" name="pass" /><br/>
-	<input type="submit" value="<?php echo $clockedin ? 'Clock Out' : 'Clock In'; ?>" />
-	<input type="submit" name="now" value="<?php echo $clockedin ? 'Clock Out NOW' : 'Clock In NOW'; ?>" />
+	<table>
+	<tr id="username_row"><td><label for="username">User: </label></td><td><input type="text" name="username" /></td></tr>
+	<?php if ( $pass_hash == $_COOKIE[ 'pass' ] ) {
+		echo "<tr id='pass_row_good'><td>You're good.</td><td></td></tr>";
+	} else {
+		echo '<tr id="pass_row"><td><label for="password">Pass: </label></td><td><input type="password" name="password" /></td></tr>';
+	} ?>
+	</table>
+	<input type="submit" class="submit" value="<?php echo $clockedin ? 'Clock Out' : 'Clock In'; ?>" />
+	<input type="submit" class="submit" name="now" value="<?php echo $clockedin ? 'Clock Out NOW' : 'Clock In NOW'; ?>" />
 </form><br/>
 <?php
 if (isset($punch_info)) {
@@ -233,13 +186,13 @@ if (isset($punch_info)) {
 	echo "<strong>Punch for {$job_info->name}</strong>: " . date("m/d/Y H:i:s", $punch_info['in']) . ' - ' . date("m/d/Y H:i:s", $punch_info['out']) . " = $hours @ {$job_info->rate}/hour = $" . number_format($earned, 2);
 } else {
 	echo "<br/>";
-}
+} 
 ?>
 <hr/>
-<form method="POST">
-	View Job Data: <select name="job">
+<form method="POST" id="reportform">
+	View Job Data: <select name="report_job">
 		<?php
-			echo $_SESSION['report_job_options'];
+			echo $job_options;
 		?>
 	</select>
 	From <input type="text" name="from_date" value="<?php echo $_SESSION['from_date']; ?>" />
@@ -249,9 +202,7 @@ if (isset($punch_info)) {
 </form>
 
 <?php
-if($_SESSION['report']) {
-	echo $_SESSION['report'];
-}
+do_report();
 
 function clock_in() {
 	global $mysqli;
@@ -310,6 +261,69 @@ function clock_out($id) {
 	success("Clocked Out&co_id=$id");
 }
 
+function do_report() {
+	global $thisjob, $from_date, $to_date, $mysqli, $status, $thisrate;
+	if ( $thisjob ) {
+		$where = "WHERE `job` = '$thisjob' AND `in` > '0' AND `out` > '0'";
+		if($from_date) {
+			$fds = explode("/", $from_date);
+			$from_date_unix = mktime(0,0,0,$fds[0],$fds[1],$fds[2]);
+			$where .= " AND `in` >= '$from_date_unix'";
+		}
+		if($to_date) {
+			$tds = explode("/", $to_date);
+			$to_date_unix = mktime(23,59,59,$tds[0],$tds[1],$tds[2]);
+			$where .= " AND `in` <= '$to_date_unix'";
+		}
+		$result = $mysqli->query("SELECT * FROM `inout` $where ORDER BY `in` ASC");
+		$jobdata = array();
+		$paidhours = $paidearned = $unpaidhours = $unpaidearned = $invoicedhours = $invoicedearned = 0;
+		while($inout = $result->fetch_assoc()) {
+			$seconds = $inout['out'] - $inout['in'];
+			$hours = $seconds / 3600;
+			$inout['hours'] = h2hm($hours);
+			$inout['earned'] = $hours * $thisrate;
+			
+			$statushours = $status[$inout['paid']] . 'hours';
+			$statusearned = $status[$inout['paid']] . 'earned';
+			
+			$jobdata[$status[$inout['paid']]][] = $inout;
+			$$statushours += $hours;
+			$$statusearned += $inout['earned'];
+		}
+
+		foreach($status as $s) {
+			$shours = $s . 'hours';
+			$searned = $s . 'earned';
+			if(count($jobdata[$s]) > 0) {
+				?>
+				<table class="report_table" border="1" cellspacing="2" cellpadding="5">
+					<tr><th colspan="4"><?php echo strtoupper($s); ?></th></tr>
+					<tr><th>In</th><th>Out</th><th>Hours</th><th>Earned</th><th>Change to...</th></tr>
+					<?php
+						foreach( $jobdata[$s] as $inout ) {
+							echo '<tr><td>' . date("m/d/Y H:i:s", $inout['in']) . '</td><td>' . date("m/d/Y H:i:s", $inout['out']) . '</td><td>' . $inout['hours'] . '</td><td>' . $inout['earned'] . '</td>';
+							echo '<td><form method="POST"><input type="hidden" name="action" value="edit_status" /><input type="hidden" name="punch_id" value="' . $inout['punch_id'] . '" />';
+							if ( 'unpaid' != $s ) {
+								echo '<input type="submit" name="status" value="unpaid" />';
+							}
+							if ( 'invoiced' != $s ) {
+								echo '<input type="submit" name="status" value="invoiced" />';
+							}
+							if ( 'paid' != $s ) {
+								echo '<input type="submit" name="status" value="paid" />';
+							}
+							echo '</form></td></tr>';
+						}
+						echo '<tr><td></td><th>Totals</th><th>' . h2hm($$shours) . '</th><th>$' . number_format($$searned, 2) . '</th></tr>';
+					?>
+				</table>
+				<?php
+			}
+		}
+	}
+}
+
 function error($error) {
 	header("Location: http://s.stevish.com/punch/?error=$error");
 	die();
@@ -338,3 +352,6 @@ function s_hash( $in ) {
 	global $sql_pass;
 	return sha1( sha1( md5( sha1( $in ) ) ) . $sql_pass );
 }
+echo "<pre>";
+print_r($_SESSION);
+print_r($_POST);
